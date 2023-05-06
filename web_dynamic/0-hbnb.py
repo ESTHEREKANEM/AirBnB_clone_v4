c2f3805b9912e3d056be04524e83026b87f7ee37
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-06 11:35:23 +0100

    remove print statements

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -26,18 +26,14 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-    print([state.name for state in states])
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-    print(amenities)
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    print(places)
-
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
